18cc7e3f1c23a05cce1238d9a7ad5b6154c98a93
Morgan Chapleau-Stein <58056433+YuzuRanger@users.noreply.github.com>
2025-10-09 21:14:58 -0700

    Setup for UO Score App

--- app/shiny/ui.R
+++ app/shiny/ui.R
@@ -1,29 +1,71 @@
-#' This is an example shiny application
-#' It is the same as example_02 in the RStudio Shiny Tutorial
-
 library(shiny)
+library(shinyjs)
+library(magick)
+library(EBImage)
+library(pdftools)
+library(collapse)
+library(stringr)
 
-ui = fluidPage(
-  titlePanel("Example Shiny App"),
+# Define UI for data upload app ----
+ui <- fluidPage(
+  useShinyjs(),
+  # App title ----
+  titlePanel("Upload Files"),
+
+  # Sidebar layout with input and output definitions ----
   sidebarLayout(
 
+    # Sidebar panel for inputs ----
     sidebarPanel(
-      selectInput(
-        inputId = "dataset",
-        label = "Choose a dataset",
-        choices = c("rock", "pressure", "cars", "iris")),
 
+      # Input: Select a file ----
+      fileInput("scanned_pdf", "Select .pdf file that holds scans of forms:",
+                multiple = FALSE,
+                accept = c(".pdf")),
+    
+      # Input: Enter an integer ----
       numericInput(
-        inputId = "obs",
-        label = "Number of observations to view:",
-        value = 10)
+                inputId = "num_questions", 
+                label = "Enter number of questions on the exam, must be 120 or less:",
+                value = 10, 
+                min = 1, 
+                max = 120, 
+                step = 1
+            ),
+
+          # Input: Enter an integer ----
+      numericInput(
+                inputId = "num_versions", 
+                label = "Enter the number of test versions:",
+                value = 1, 
+                min = 1, 
+                max = 10, 
+                step = 1
+            ),
+    
+          # Input: Select a file ----
+      fileInput("answer_key", "Select .csv file that holds answer key for exam:",
+                multiple = FALSE,
+                accept = c(".csv")),
+
+      # Horizontal line ----
+      tags$hr(),
+      
+
+      # Button
+      actionButton("processData", "Process Data")
+
     ),
 
+    # Main panel for displaying outputs ----
     mainPanel(
-      tags$div(sprintf("Global Variable Value: %s", GLOBAL_VAR)),
-      verbatimTextOutput("commandArgs"),
-      verbatimTextOutput("dataSummary"),
-      tableOutput("dataView")
+      verbatimTextOutput("updates"),
+      textOutput("fail_list"),
+      shinyjs::hidden(downloadButton("downloadData", "Download Processed Student Answers")),
+      shinyjs::hidden(downloadButton("downloadScores", "Download Processed Student Scores")),
+      shinyjs::hidden(downloadButton("downloadAlerts", "Download Alerts/Irregularities in Student Responses"))
+
     )
+
   )
 )
